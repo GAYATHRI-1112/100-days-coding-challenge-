28d5ad5ad07438ca8d2123ce6612b1cbc4b01ba4
GAYATHRI-1112 <137431672+GAYATHRI-1112@users.noreply.github.com>
2023-07-03 21:57:34 +0530

    Create Day 11.c

---
+++ Day 11.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int T,N;
+    printf("Enter the no of text cases=");
+    scanf("%d",&T);
+ while(T--)
+    {
+        printf("Enter the weight of pulp =");
+        scanf("%d",&N);
+        printf("The no of notebooks=%d\n",N*10);
+    }
+
+}
